afb278d3ba708bdd43a9fada021c48aa01895eb7
samaralsherbeny <samaralsherbeny2@gmail.com>
2026-08-09 17:29:50 +0300

    Create app_ar.ts

---
+++ task3-smarthome/app_ar.ts
@@ -0,0 +1,135 @@
+<?xml version="1.0" encoding="utf-8"?>
+<!DOCTYPE TS>
+<TS version="2.1" language="ar_EG">
+<context>
+    <name>Main</name>
+    <message>
+        <source>Smart Home Dashboard</source>
+        <translation>لوحة التحكم بالمنزل الذكي</translation>
+    </message>
+</context>
+<context>
+    <name>LoginPage</name>
+    <message>
+        <source>Smart Home</source>
+        <translation>المنزل الذكي</translation>
+    </message>
+    <message>
+        <source>Control your smart home</source>
+        <translation>تحكم في منزلك الذكي</translation>
+    </message>
+    <message>
+        <source>Username</source>
+        <translation>اسم المستخدم</translation>
+    </message>
+    <message>
+        <source>Password</source>
+        <translation>كلمة المرور</translation>
+    </message>
+    <message>
+        <source>Login</source>
+        <translation>تسجيل الدخول</translation>
+    </message>
+</context>
+<context>
+    <name>DashboardPage</name>
+    <message>
+        <source>Welcome Home</source>
+        <translation>مرحباً بك في المنزل</translation>
+    </message>
+    <message>
+        <source>Smart Home Dashboard</source>
+        <translation>لوحة التحكم بالمنزل الذكي</translation>
+    </message>
+    <message>
+        <source>Home Connected</source>
+        <translation>المنزل متصل</translation>
+    </message>
+    <message>
+        <source>WiFi Status</source>
+        <translation>حالة الواي فاي</translation>
+    </message>
+    <message>
+        <source>Connected</source>
+        <translation>متصل</translation>
+    </message>
+    <message>
+        <source>Devices Online</source>
+        <translation>الأجهزة المتصلة</translation>
+    </message>
+    <message>
+        <source>Energy Usage</source>
+        <translation>استهلاك الطاقة</translation>
+    </message>
+    <message>
+        <source>Normal</source>
+        <translation>طبيعي</translation>
+    </message>
+    <message>
+        <source>Smart Devices</source>
+        <translation>الأجهزة الذكية</translation>
+    </message>
+</context>
+<context>
+    <name>DeviceCard</name>
+    <message>
+        <source>Living Room Light</source>
+        <translation>إضاءة غرفة المعيشة</translation>
+    </message>
+    <message>
+        <source>Bedroom Light</source>
+        <translation>إضاءة غرفة النوم</translation>
+    </message>
+    <message>
+        <source>Air Conditioner</source>
+        <translation>تكييف الهواء</translation>
+    </message>
+    <message>
+        <source>Fan</source>
+        <translation>المروحة</translation>
+    </message>
+    <message>
+        <source>Garage Door</source>
+        <translation>باب المرآب</translation>
+    </message>
+    <message>
+        <source>Online</source>
+        <translation>متصل</translation>
+    </message>
+    <message>
+        <source>Battery</source>
+        <translation>البطارية</translation>
+    </message>
+</context>
+<context>
+    <name>SettingsPage</name>
+    <message>
+        <source>Settings</source>
+        <translation>الإعدادات</translation>
+    </message>
+    <message>
+        <source>Language</source>
+        <translation>اللغة</translation>
+    </message>
+    <message>
+        <source>Brightness</source>
+        <translation>السطوع</translation>
+    </message>
+    <message>
+        <source>Temperature</source>
+        <translation>درجة الحرارة</translation>
+    </message>
+    <message>
+        <source>Enable Notifications</source>
+        <translation>تفعيل الإشعارات</translation>
+    </message>
+    <message>
+        <source>Save</source>
+        <translation>حفظ</translation>
+    </message>
+    <message>
+        <source>Back</source>
+        <translation>رجوع</translation>
+    </message>
+</context>
+</TS>

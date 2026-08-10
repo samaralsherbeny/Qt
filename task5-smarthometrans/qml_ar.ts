61bca71b9fd713bf3ca645741beb2b5f5d62bf1d
samaralsherbeny <samaralsherbeny2@gmail.com>
2026-08-10 15:57:16 +0300

    Create qml_ar.ts

---
+++ task5-smarthometrans/qml_ar.ts
@@ -0,0 +1,189 @@
+<?xml version="1.0" encoding="utf-8"?>
+<!DOCTYPE TS>
+<TS version="2.1" language="ar">
+<context>
+    <name>DashboardPage</name>
+    <message>
+        <location filename="../DashboardPage.qml" line="173"/>
+        <source>Welcome Home</source>
+        <translation>مرحبًا بك في المنزل</translation>
+    </message>
+    <message>
+        <location filename="../DashboardPage.qml" line="185"/>
+        <source>Smart Home Dashboard</source>
+        <translation>لوحة تحكم المنزل الذكي</translation>
+    </message>
+    <message>
+        <location filename="../DashboardPage.qml" line="240"/>
+        <source>🏠 Home Connected</source>
+        <translation>🏠 المنزل متصل</translation>
+    </message>
+    <message>
+        <location filename="../DashboardPage.qml" line="257"/>
+        <source>WiFi Status : Connected</source>
+        <translation>حالة WiFi: متصل</translation>
+    </message>
+    <message>
+        <location filename="../DashboardPage.qml" line="271"/>
+        <source>Devices Online : 5</source>
+        <translation>الأجهزة المتصلة: 5</translation>
+    </message>
+    <message>
+        <location filename="../DashboardPage.qml" line="285"/>
+        <source>Energy Usage : Normal</source>
+        <translation>استهلاك الطاقة: طبيعي</translation>
+    </message>
+    <message>
+        <location filename="../DashboardPage.qml" line="305"/>
+        <source>Smart Devices</source>
+        <translation>الأجهزة الذكية</translation>
+    </message>
+    <message>
+        <location filename="../DashboardPage.qml" line="344"/>
+        <source>Living Room Light</source>
+        <translation>مصباح غرفة المعيشة</translation>
+    </message>
+    <message>
+        <location filename="../DashboardPage.qml" line="347"/>
+        <source>Bedroom Light</source>
+        <translation>مصباح غرفة النوم</translation>
+    </message>
+    <message>
+        <location filename="../DashboardPage.qml" line="350"/>
+        <source>Air Conditioner</source>
+        <translation>مكيف الهواء</translation>
+    </message>
+    <message>
+        <location filename="../DashboardPage.qml" line="353"/>
+        <source>Fan</source>
+        <translation>المروحة</translation>
+    </message>
+    <message>
+        <location filename="../DashboardPage.qml" line="356"/>
+        <source>Garage Door</source>
+        <translation>باب المرآب</translation>
+    </message>
+    <message>
+        <location filename="../DashboardPage.qml" line="406"/>
+        <source>🏠 Home</source>
+        <translation>🏠 الرئيسية</translation>
+    </message>
+    <message>
+        <location filename="../DashboardPage.qml" line="459"/>
+        <source>⚙ Settings</source>
+        <translation>⚙ الإعدادات</translation>
+    </message>
+</context>
+<context>
+    <name>DeviceCard</name>
+    <message>
+        <location filename="../DeviceCard.qml" line="140"/>
+        <source>🟢 Online</source>
+        <translation>🟢 متصل</translation>
+    </message>
+    <message>
+        <location filename="../DeviceCard.qml" line="141"/>
+        <source>🔴 Offline</source>
+        <translation>🔴 غير متصل</translation>
+    </message>
+    <message>
+        <location filename="../DeviceCard.qml" line="199"/>
+        <source>Temperature</source>
+        <translation>درجة الحرارة</translation>
+    </message>
+    <message>
+        <location filename="../DeviceCard.qml" line="205"/>
+        <source>Speed</source>
+        <translation>السرعة</translation>
+    </message>
+    <message>
+        <location filename="../DeviceCard.qml" line="211"/>
+        <source>Position</source>
+        <translation>الموضع</translation>
+    </message>
+    <message>
+        <location filename="../DeviceCard.qml" line="214"/>
+        <source>Brightness</source>
+        <translation>السطوع</translation>
+    </message>
+</context>
+<context>
+    <name>LoginPage</name>
+    <message>
+        <location filename="../LoginPage.qml" line="85"/>
+        <source>Smart Home</source>
+        <translation>المنزل الذكي</translation>
+    </message>
+    <message>
+        <location filename="../LoginPage.qml" line="100"/>
+        <source>Control your smart home</source>
+        <translation>تحكم في منزلك الذكي</translation>
+    </message>
+    <message>
+        <location filename="../LoginPage.qml" line="124"/>
+        <source>Username</source>
+        <translation>اسم المستخدم</translation>
+    </message>
+    <message>
+        <location filename="../LoginPage.qml" line="164"/>
+        <source>Password</source>
+        <translation>كلمة المرور</translation>
+    </message>
+    <message>
+        <location filename="../LoginPage.qml" line="225"/>
+        <source>Login</source>
+        <translation>تسجيل الدخول</translation>
+    </message>
+    <message>
+        <location filename="../LoginPage.qml" line="261"/>
+        <source>Please fill all fields</source>
+        <translation>يرجى ملء جميع الحقول</translation>
+    </message>
+</context>
+<context>
+    <name>Main</name>
+    <message>
+        <location filename="../Main.qml" line="16"/>
+        <source>Smart Home Dashboard</source>
+        <translation>لوحة تحكم المنزل الذكي</translation>
+    </message>
+</context>
+<context>
+    <name>SettingsPage</name>
+    <message>
+        <location filename="../SettingsPage.qml" line="114"/>
+        <source>⚙ Settings</source>
+        <translation>⚙ الإعدادات</translation>
+    </message>
+    <message>
+        <location filename="../SettingsPage.qml" line="155"/>
+        <source>🌐 Language</source>
+        <translation>🌐 اللغة</translation>
+    </message>
+    <message>
+        <location filename="../SettingsPage.qml" line="223"/>
+        <source>🔔 Enable Notifications</source>
+        <translation>🔔 تفعيل الإشعارات</translation>
+    </message>
+    <message>
+        <location filename="../SettingsPage.qml" line="286"/>
+        <source>☀ Brightness</source>
+        <translation>☀ السطوع</translation>
+    </message>
+    <message>
+        <location filename="../SettingsPage.qml" line="372"/>
+        <source>🌡 Temperature</source>
+        <translation>🌡 درجة الحرارة</translation>
+    </message>
+    <message>
+        <location filename="../SettingsPage.qml" line="446"/>
+        <source>Cancel</source>
+        <translation>إلغاء</translation>
+    </message>
+    <message>
+        <location filename="../SettingsPage.qml" line="490"/>
+        <source>💾 Save Changes</source>
+        <translation>💾 حفظ التغييرات</translation>
+    </message>
+</context>
+</TS>

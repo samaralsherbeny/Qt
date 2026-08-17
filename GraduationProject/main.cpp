ba5d360cf43f043eae49e90c599f77bd4655f252
samaralsherbeny <samaralsherbeny2@gmail.com>
2026-08-18 01:41:48 +0300

    Update main.cpp

--- GraduationProject/main.cpp
+++ GraduationProject/main.cpp
@@ -1,18 +1,61 @@
 #include <QGuiApplication>
 #include <QQmlApplicationEngine>
+#include <QQmlContext>
+
+#include "radiostationmodel.h"
+#include "mediabackend.h"
 
 int main(int argc, char *argv[])
 {
     QGuiApplication app(argc, argv);
 
     QQmlApplicationEngine engine;
+
+    // =====================================================
+    // RADIO BACKEND
+    // =====================================================
+
+    RadioStationModel radioModel;
+
+    engine.rootContext()->setContextProperty(
+        "radioModel",
+        &radioModel
+        );
+
+    // =====================================================
+    // MEDIA BACKEND
+    // =====================================================
+
+    MediaBackend mediaBackend;
+
+    engine.rootContext()->setContextProperty(
+        "mediaBackend",
+        &mediaBackend
+        );
+
+    // =====================================================
+    // QML ERROR HANDLING
+    // =====================================================
+
     QObject::connect(
         &engine,
         &QQmlApplicationEngine::objectCreationFailed,
         &app,
-        []() { QCoreApplication::exit(-1); },
-        Qt::QueuedConnection);
-    engine.loadFromModule("MediaPlayerIVI", "Main");
+        []()
+        {
+            QCoreApplication::exit(-1);
+        },
+        Qt::QueuedConnection
+        );
 
-    return QGuiApplication::exec();
+    // =====================================================
+    // LOAD MAIN QML
+    // =====================================================
+
+    engine.loadFromModule(
+        "MediaPlayerIVI",
+        "Main"
+        );
+
+    return app.exec();
 }
